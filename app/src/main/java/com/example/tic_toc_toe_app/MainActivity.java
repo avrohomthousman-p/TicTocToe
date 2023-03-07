42c3c357228d070eac47edc3d33e209c2c87e28d
avrohomthousman <avrohomthousman@gmail.com>
2023-03-07 12:00:23 -0500

    Setup buttons in main activity.

--- app/src/main/java/com/example/tic_toc_toe_app/MainActivity.java
+++ app/src/main/java/com/example/tic_toc_toe_app/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.tic_toc_toe_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupButtonListeners();
+    }
+
+
+    /**
+     * Creates the click handlers for the two buttons on the main activity screen.
+     */
+    private void setupButtonListeners(){
+        findViewById(R.id.pvp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO setup model and start new activity
+            }
+        });
+
+
+        findViewById(R.id.pvc).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO setup model and start new activity
+            }
+        });
     }
 }
