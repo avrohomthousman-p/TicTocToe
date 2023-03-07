895d57f73847306bcb6ae1fb5ff07595c1a7f386
avrohomthousman <avrohomthousman@gmail.com>
2023-03-07 12:13:27 -0500

    Created new activity to run game

--- app/src/main/java/com/example/tic_toc_toe_app/MainActivity.java
+++ app/src/main/java/com/example/tic_toc_toe_app/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tic_toc_toe_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,7 +25,9 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.pvp).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO setup model and start new activity
+                //TODO setup model
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                MainActivity.this.startActivity(intent);
             }
         });
 
@@ -32,7 +35,9 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.pvc).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO setup model and start new activity
+                //TODO setup model
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                MainActivity.this.startActivity(intent);
             }
         });
     }
